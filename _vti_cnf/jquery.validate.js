41ebd9464836239b23c600d13a5d43e885cda9f3
Michael Tigue <mtigue@web4uonline.com>
2013-02-02 09:20:42 -0500

    19

--- _vti_cnf/jquery.validate.js
+++ _vti_cnf/jquery.validate.js
@@ -6,6 +6,6 @@ vti_modifiedby:SR|web4u\\mtigue
 vti_timecreated:TR|16 Jan 2013 21:32:00 -0000
 vti_cacheddtm:TX|16 Jan 2013 21:32:00 -0000
 vti_filesize:IR|39321
-vti_backlinkinfo:VX|app/9/request-certificate.html app/9/not\\ used\\ html/test.html
+vti_backlinkinfo:VX|app/9/index.html app/9/request-certificate.html app/9/request-quote.html app/9/index-works-rakhi.html app/9/not\\ used\\ html/test.html app/works-raki/index-works-rakhi.html
 vti_syncofs_208.109.14.76\:21/schantz:TW|01 Feb 2013 02:25:25 -0000
 vti_syncwith_208.109.14.76\:21/schantz:TX|16 Jan 2013 21:32:00 -0000
