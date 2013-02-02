b7cae136296737dd27fd2694dcdd4966089513d0
Michael Tigue <mtigue@web4uonline.com>
2013-02-02 16:36:59 -0500

    20

---
+++ _vti_cnf/jquery.ui.datepicker.mobile.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 Feb 2013 20:28:14 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timecreated:TR|02 Feb 2013 20:28:14 -0000
+vti_cacheddtm:TX|02 Feb 2013 20:28:14 -0000
+vti_filesize:IR|2117
+vti_backlinkinfo:VX|
+vti_syncofs_208.109.14.76\:21/schantz:TW|02 Feb 2013 20:34:53 -0000
+vti_syncwith_208.109.14.76\:21/schantz:TX|02 Feb 2013 20:28:14 -0000
